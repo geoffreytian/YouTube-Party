17893ed1abc67c008998243f0e35e48e41b53f70
geoffreytian <geoffreytian@utexas.edu>
2020-06-20 11:44:32 -0500

    working client-server, need to figure out event logic

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
-const path = require('path')
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 const PORT = process.env.PORT || 5000
+const path = require('path')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/test', (req, res) => res.send(printHello()))
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-printHello = () => {
-  console.log("hello")
-}
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('playEvent', function(msg) {
+        //console.log('play ' + msg);
+        var newReq = msg.b;
+        socket.broadcast.emit('play', msg);
+    });
+    socket.on('pauseEvent', function(msg) {
+        //console.log('pause: ' + msg);
+        var newReq = msg.b;
+        socket.broadcast.emit('pause', msg);
+    });
+    socket.on('bufferEvent', function(msg) {
+        //console.log('buffer: ' + msg);
+        var newReq = msg.b;
+        socket.broadcast.emit('buffer', msg);
+    });
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+    });
+});
